89a7ec85b74c83f5d3200e4eeb562b3288552b33
oberdev <qovalski@gmail.com>
2021-01-11 11:34:57 +0300

    fix view of event notifications

--- src/utils/notify.ts
+++ src/utils/notify.ts
@@ -34,10 +34,10 @@ const buildRegularNotification = (notifications: RegularNotification[], header: 
 
 const buildEventNotification = (notification: EventNotification, header: string): string => {
   let text = '<u>СОБЫТИЙНОЕ УВЕДОМЛЕНИЕ</u>\n'
-  text += `Описание: ${notification.description}\n`
-  text += header
   text += `<b>${notification.label}</b>:`
-  text += `${notification.value} ${notification.measure} ${HEARTS[notification.color] ? HEARTS[notification.color] : ''}`
+  text += `${notification.value} ${notification.measure} ${HEARTS[notification.color] ? HEARTS[notification.color] : ''}\n`
+  // text += `Описание: ${notification.description}\n`
+  text += header
   text += '\n'
   return text
 }
